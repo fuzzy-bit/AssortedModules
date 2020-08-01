9f35bf681e571710e355a9c84544373f77f498b7
SignorOmbra <65546939+SignorOmbra@users.noreply.github.com>
2020-08-01 06:00:24 -0400

    Was in a rush, shipped broken code last commit.

--- Modules/Music.lua
+++ Modules/Music.lua
@@ -1,22 +1,25 @@
+--DEFINE MODULE--
 local MusicModule = {}
 
 
 
 --DEFINE SERVICES--
-local TweenService = game:GetService("TweenService")
 local MarketplaceService = game:GetService("MarketplaceService")
+local ReplicatedStorage = game.ReplicatedStorage
 
 
 
 --CHECK EXISTANCE OF OBJECTS--
-if not script:FindFirstChild("Resources") then
+local PlaybackObject = workspace:FindFirstChild("Music")
+
+if not ReplicatedStorage:FindFirstChild("Resources") then
 	error("No 'Resources' folder!")
-elseif script:FindFirstChild("Resources") and not script:FindFirstChild("Resources"):FindFirstChild("MusicList") then
+elseif ReplicatedStorage:FindFirstChild("Resources") and not ReplicatedStorage:FindFirstChild("Resources"):FindFirstChild("MusicList") then
 	error("No 'MusicList' folder!")
 end
 
 if not workspace:FindFirstChild("Music") then
-	local PlaybackObject = Instance.new("Sound")
+	PlaybackObject = Instance.new("Sound")
 	PlaybackObject.Parent = workspace
 	PlaybackObject.Name = "Music"
 end
@@ -24,7 +27,7 @@ end
 
 
 --LOCAL VARIABLES--
-local Resources = script.Resources
+local Resources = ReplicatedStorage.Resources
 local Music = workspace.Music
 local MusicList = Resources.MusicList
 
@@ -34,32 +37,34 @@ local NewSong = nil
 
 
 --FUNCTIONS--
+function MusicModule:PlaySong(Song)
+	LastSong = Song
+
+	PlaybackObject.SoundId = Song.SoundId
+	PlaybackObject.PlaybackSpeed = Song.PlaybackSpeed
+
+	if PlaybackObject.Playing then
+		PlaybackObject:Stop()
+	end
+
+	PlaybackObject:Play()
+
+	local SoundId = string.sub(NewSong.SoundId, 14)
+	local Asset = MarketplaceService:GetProductInfo(SoundId)
+
+	return PlaybackObject
+end
+
 function MusicModule:PlayRandomSong()
 	local Songs = MusicList:GetChildren()
 	if #Songs >= 2 then
 		repeat NewSong = Songs[math.random(1, #Songs)] until NewSong ~= LastSong and NewSong:IsA("Sound")
 		
-		LastSong = NewSong
-		
-		Music.SoundId = NewSong.SoundId
-		Music.PlaybackSpeed = NewSong.PlaybackSpeed
-	
-		if NewSong:FindFirstChild("StartTime") then
-			script.Music.TimePosition = NewSong.StartTime.Value
-		end
-		
-		if Music.Playing then
-			Music:Stop()
-		end
-		
-		Music:Play()
-		
-		local SoundId = string.sub(NewSong.SoundId, 14)
-		local Asset = MarketplaceService:GetProductInfo(SoundId)
-		
-		return Music
+		MusicModule:PlaySong(NewSong)
+
+		return PlaybackObject
 	else
-		error("MusicList must have two or more songs!")
+		warn("MusicList must have two or more songs!")
 	end
 end
 
@@ -69,4 +74,5 @@ end
 
 
 
+--RETURN MODULE--
 return MusicModule
